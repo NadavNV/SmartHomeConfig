9663662d5afc094143f77acb07d909dfeafc41f4
NadavNV <nadav.nevo@gmail.com>
2025-07-22 16:51:38 +0300

    Added entrypoint script

---
+++ monitoring/grafana/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Start run.sh in background and log output
+/run.sh >> /proc/1/fd/1 2>&1 &
+
+# Run startup.sh in foreground so we see its logs
+/startup.sh >> /proc/1/fd/1 2>&1
+
+# Wait for background job to finish
+wait
